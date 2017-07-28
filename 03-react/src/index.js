7011139ba42bc3825c4b0f865b941d41a4815dd5
Parn <parnpresso@gmail.com>
2017-07-28 11:54:17 +0700

    Added 2 component to app

--- 03-react/src/index.js
+++ 03-react/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Title = () => (
+    <h1>TITLE</h1>
+)
+
 const App = () => (
-    <h1>Hello, React</h1>
+    <div>
+        <Title />
+        <h2>Hello, React</h2>
+    </div>
 ) 
 
 ReactDOM.render(
