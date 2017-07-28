8251d17b4a6a9c27fb9e52ec5db3b144e2827017
Parn <parnpresso@gmail.com>
2017-07-28 16:52:51 +0700

    Init file for redux

---
+++ 07-redux/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Nav from './Nav'
+import MainPanel from './MainPanel'
+
+
+const App = () => (
+  <div>
+    <Nav />
+    <MainPanel />
+  </div>
+)
+export default App
